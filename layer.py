a8b8824412938e28934fd18ba112131a7e603a8e
Lburris12 <63323671+Lburris12@users.noreply.github.com>
2021-04-17 23:21:38 -0400

    Update layer.py

--- layer.py
+++ layer.py
@@ -2,17 +2,21 @@ import numpy as np
 from activation import sigmoid, leakyRelu
 
 class SigmoidLayer:
+    
     def __init__(self, num_inputs, num_neurons):
         self.weights = 0.1 * np.random.randn(num_inputs, num_neurons)
         self.biases = np.zeros((1, num_neurons))
+        
     def forward_prop(self, inputs):
         z = np.dot(inputs, self.weights) + self.biases
         self.out = sigmoid(z)
 
 class ReluLayer:
+    
     def __init__(self, num_inputs, num_neurons):
         self.weights = 0.1 * np.random.randn(num_inputs, num_neurons)
         self.biases = np.zeros((1, num_neurons))
+        
     def forward_prop(self, inputs):
         z = np.dot(inputs, self.weights) + self.biases
         self.out = leakyRelu(z)
