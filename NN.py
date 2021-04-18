7bb1c080ff5c6dafc41dcf6b4789a7057a62e75a
Lburris12 <63323671+Lburris12@users.noreply.github.com>
2021-04-17 23:20:02 -0400

    cleaned up neural network code and added comments

--- NN.py
+++ NN.py
@@ -3,6 +3,7 @@ from activation import sigmoidDerivative, leakyReluDerivative
 import numpy as np
 
 def convert_labels(labels):
+    #convert label into an array 
     variance = set(labels)
     new_labels = [[0 for i in range(len(variance))] for j in range(len(labels))]
     for i, label in enumerate(labels):
@@ -21,6 +22,8 @@ class NeuralNetwork:
             self.layers = [ReluLayer(n_inputs, n_neurons) for n_inputs, n_neurons in layer_details]
 
     def forward_prop(self, inputs):
+        #propogate data forward through network
+        
         out = inputs
         for layer in self.layers:
             layer.forward_prop(out)
@@ -28,24 +31,35 @@ class NeuralNetwork:
         self.out = out
     
     def back_prop(self, labels, learning_rate):
+        #update weights and biases of neural network
         alter_values = []
         prev_layer = None
         prev_dc_dz = None
+        
         for i, layer in enumerate(reversed(self.layers[1:])):
+            
             if self.activation_function == 'relu':
                 func_derivative = leakyReluDerivative(layer.out)
+                
             elif self.activation_function == 'sigmoid':
                 func_derivative = sigmoidDerivative(layer.out)
             
             if i == 0:
                 dc_dz = np.multiply(2 * (layer.out-labels), func_derivative)
+                
             else:
                 dc_dcl = np.sum(prev_dc_dz * prev_layer.weights, axis=1)
                 dc_dz = dc_dcl * func_derivative
+                #compute the partial derivative of the cost function with repect to the biases of the current layer
+                
             dc_dw = np.dot(self.layers[-(i+2)].out.T, dc_dz)
+            #compute the partial derivative of the cost function with respect to the weights of the current layer
+            
             alter_values.insert(0, [])
             alter_values[0].append(learning_rate * dc_dw)
             alter_values[0].append(learning_rate * dc_dz)
+            #store these values
+            
             prev_dc_dz = dc_dz
             prev_layer = layer
         
@@ -53,14 +67,17 @@ class NeuralNetwork:
             old = layer.weights
             layer.weights -= values[0]
             layer.biases -= values[1]
+            #update weights and biases
           
     
     def fit(self, inputs, labels, learning_rate, iters):
         labels = convert_labels(labels)
+        #convert labels to arrays in order to be compared to the output layer of the neural network
 
         length = len(inputs)
         for i in range(iters):
             for j in range(length):
+                
                 self.forward_prop(inputs[j])
                 self.back_prop(labels[j], learning_rate)
     
@@ -69,9 +86,15 @@ class NeuralNetwork:
         self.prediction = np.argmax(self.out)
     
     def test(self, inputs, labels):
+        #get accuracy of current model
+        
         correct = 0
+        
         for i in range(len(inputs)):
+            
             self.predict(inputs[i])
+            
             if self.prediction == labels[i]:
                 correct += 1
+                
         self.accuracy = str(correct/len(inputs))[:4]
