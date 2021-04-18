3c72fd33355486481b4f24f933a7d3775317f933
Lburris12 <63323671+Lburris12@users.noreply.github.com>
2021-04-17 23:25:07 -0400

    Update classify.py

--- classify.py
+++ classify.py
@@ -4,10 +4,18 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
 data = load_iris()
+#import data
+
 x_train, x_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.33)
+#split data into  67% training data and 33% testing data
 
 model = NeuralNetwork([[4,4],[4,5],[5,3]], 'sigmoid')
-model.fit(x_train, y_train, 0.5, 1000)
-model.test(x_test, y_test)
+#initialize model with three layers
 
-print(model.accuracy)
+model.fit(x_train, y_train, 0.5, 1000)
+#train model
+
+model.test(x_test, y_test)
+#test model
+
+print(f'accuracy: {model.accuracy}')
